322f9ced46c9e0cb749cabd421de89555e955fae
niru9711 <nirusanant97@gmail.com>
2025-03-24 23:53:53 +0530

    views

--- chatbot_app/views.py
+++ chatbot_app/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
-# Create your views here.
-
+from django.contrib.auth.decorators import login_required
 
 
-def index(request):
+
+@login_required
+def chat_page(request):
+    return render(request, 'chat.html')
+
+def login_view(request):
     template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+def signout(request):
+    return 
+
+def signup_view(request):
+    template = loader.get_template('signup.html')
     return HttpResponse(template.render())
