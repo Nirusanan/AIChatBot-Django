c5350ce8af6cce181c6f94dafa45b111ca0a9258
niru9711 <nirusanant97@gmail.com>
2025-03-24 23:54:04 +0530

    url

--- chatbot_app/urls.py
+++ chatbot_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from . import views
+from .views import chat_page, login_view, signup_view, signout
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', login_required(chat_page), name='chat_page'),
+    path('login/', login_view, name="login"),
+    path("signup", signup_view, name="signup"),
+    path('signout', signout, name='signout'),
 ]
