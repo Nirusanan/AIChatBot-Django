5ad8ac89ab26723dad7bcfdaafa792c90c791e33
niru9711 <nirusanant97@gmail.com>
2025-03-25 23:07:12 +0530

    settings

--- django_chatbot/settings.py
+++ django_chatbot/settings.py
@@ -122,7 +122,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+LOGIN_URL = '/login/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'chatbot_app.CustomUser'
