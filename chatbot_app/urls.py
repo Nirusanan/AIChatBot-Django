a66889c2883e50bd26f0d52314ef4b3d2105cf94
niru9711 <nirusanant97@gmail.com>
2025-03-27 20:15:19 +0530

    chat-url

--- chatbot_app/urls.py
+++ chatbot_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import chat_page, login_view, signup_view, logout_view
+from .views import chat_page, login_view, signup_view, logout_view, get_chat_sessions, get_chat_messages, chat_response 
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
@@ -7,4 +7,7 @@ urlpatterns = [
     path('login/', login_view, name="login"),
     path("signup/", signup_view, name="signup"),
     path('logout/', logout_view, name='logout'),
+    path('api/chat_id_title/', get_chat_sessions, name='get_all_chats_id_title'),
+    path('api/chat/<uuid:chat_uuid>/', get_chat_messages, name='chat_details_api'),
+    path('api/chat/', chat_response, name='chat_api')
 ]
