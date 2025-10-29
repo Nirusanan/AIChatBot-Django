033444a1734e763c775d131f03d7a713bed871d8
niru9711 <nirusanant97@gmail.com>
2025-10-29 18:13:19 +0530

    Models

--- chatbot_app/models.py
+++ chatbot_app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 import uuid
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import send_mail
+
 
 class CustomUserManager(BaseUserManager):
     """Manager for CustomUser model"""
@@ -27,7 +31,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     """Custom User Model with username-based authentication."""
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
     objects = CustomUserManager()
@@ -41,19 +45,48 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
 class ChatSession(models.Model):
     chat_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="chat_sessions",
+        null=False,  
+        blank=False
+    )
     chat_title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField()
 
     def __str__(self):
-        return self.chat_title
+        return f"{self.chat_title} ({self.user.username})"
 
 
 class ChatMessage(models.Model):
-    chat = models.ForeignKey(ChatSession, on_delete=models.CASCADE)
+    chat = models.ForeignKey(ChatSession, on_delete=models.CASCADE, related_name="messages")
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="user_messages", null=False, blank=False)
     user_input = models.TextField()
     response = models.TextField()
     time_stamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Message from {self.time_stamp}"
+        return f"Message by {self.user.username} on {self.time_stamp}"
+    
+
+@receiver(post_save, sender=CustomUser)
+def send_registration_email(sender, instance, created, **kwargs):
+    """Send email only when a new user is created."""
+    if created:
+        subject = "Account has been registered"
+        message = (
+            f"Hi {instance.username},\n\n"
+            "Thank you for registering with us.\n"
+            "Your account is currently pending admin approval.\n"
+            "You’ll receive another email once your account is activated.\n\n"
+            "Best regards,\nAdmin Team"
+        )
+        send_mail(
+            subject,
+            message,
+            'admin@gmail.com',
+            [instance.email],
+            fail_silently=False,
+        )
